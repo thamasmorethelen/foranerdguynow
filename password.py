6e98b1272a231e5240dea79aeff6a334e8675381
Thomas Thelen <thamasthelen@gmail.com>
2022-11-10 10:29:55 -0800

    removed comments and fiddled around with sizing

--- password.py
+++ password.py
@@ -1,20 +1,20 @@
-# Password Generator a program
 from password_genny import password_generator
-# Function that captures user input
+
+
 def user_num():
     user_input = ''
     while True:
         try:
-            user_input = int(input('Please enter an integer greater than 10: '))
+            user_input = int(input('Please enter an integer greater than 12: '))
             return user_input
         except ValueError:
-            input('Please enter an integer (whole number) greater than 10')
+            input('Please enter an number greater than 12')
 
 
 # Function that contains main while loop
 def app():
     num = 0
-    while num < 10:
+    while num < 12:
         num = user_num()
     print(password_generator(num))
 
