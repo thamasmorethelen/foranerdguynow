1565f73bcfba685f07ea188b1c18af9ff18b9e04
Thomas Thelen <thamasthelen@gmail.com>
2022-11-10 19:38:56 -0800

    TODOs

--- password_genny_app/password_tinker.py
+++ password_genny_app/password_tinker.py
@@ -2,6 +2,10 @@ from tkinter import *
 import random
 import string
 
+#TODO add input for account name
+#TODO add input to collect num of uppercase letters
+#TODO add input to collect num of special characters
+#TODO write account name and password to txt- use a class base context manager for funsies!
 
 window = Tk()
 
@@ -21,15 +25,17 @@ def password_generator():
 
 
 generator_btn = Button(window, text="Generate Password", command=password_generator)
-generator_btn.grid(row=1, column=0)
+generator_btn.grid(row=1, column=1)
 
 
 user_num = StringVar()
 user_input = Entry(window, textvariable=user_num, width=18)
-user_input.grid(row=0, column=0)
+user_input.grid(row=0, column=1)
+# input_label = Label(window, text="Enter the number characters you want your password to be ==>")
+# input_label.grid(row=0, column=1)
 
 pass_wig = Text(window, height=1, width=20)
-pass_wig.grid(row=2, column=0)
+pass_wig.grid(row=2, column=1)
 
 
 def app():
